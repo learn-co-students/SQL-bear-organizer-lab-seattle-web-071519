c8e255c5e03e4d10ca1390cc1d84ea0f4be90c6b
marshallhelenm <marshallhelenm@gmail.com>
2019-07-23 17:41:11 -0700

    Done.

---
+++ lib/create.sql
@@ -0,0 +1,19 @@
+CREATE TABLE bears (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  age INTEGER,
+  gender TEXT,
+  color TEXT,
+  temperament TEXT,
+  alive BOOLEAN
+);
+
+
+
+-- id	integer
+-- name	text
+-- age	integer
+-- gender	text
+-- color	text
+-- temperament	text
+-- alive	boolean
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 15, "M", "brown", "friendly", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 5, "F", "brown", "rambunctious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 8, "F", "black", "serious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 18, "F", "black", "serious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 6, "F", "white", "friendly", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 16, "M", "green", "grouchy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 22, "F", "tan", "rambunctious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 4, "M", "FIERY RED", "ANGRY", 1);
